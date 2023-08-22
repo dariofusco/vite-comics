a06cef35fd576a2ad90608b2f6b37e71874b9295
dariofusco <fdario@hotmail.it>
2023-08-22 14:46:14 +0200

    genera dinamicamente link navbar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,34 @@
 <script>
 export default {
-
+    data() {
+        return {
+            navLinks: [
+                { name: "CHARACTERS" },
+                { name: "COMICS" },
+                { name: "MOVIES" },
+                { name: "TV" },
+                { name: "GAMES" },
+                { name: "COLLECTIBLES" },
+                { name: "VIDEOS" },
+                { name: "FANS" },
+                { name: "NEWS" },
+            ],
+        };
+    },
 };
 </script>
 
 <template>
-<header></header>
+    <header>
+        <img src="../assets/img/dc-logo.png" alt="">
+        <ul>
+            <li v-for="link in navLinks">
+                <a href="#" :class="{ active: link.name === 'COMICS' }">
+                    {{ link.name }}
+                </a>
+            </li>
+        </ul>
+    </header>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
