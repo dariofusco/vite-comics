e4f366de0af9ba41045948e939af115b10b58718
dariofusco <fdario@hotmail.it>
2023-08-23 14:34:07 +0200

    inserisce hero section

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,14 @@ export default {
 <template>
   <TheHeader></TheHeader>
   <main>
-    <div class="container py-4">
+    <div class="hero"></div>
+    <div class="container text-center py-4">
       <div class="row">
         <div class="col" v-for="comic in comicsList">
           <ComicsCard :imgSrc="comic.thumb" :comicsType="comic.type"></ComicsCard>
         </div>
       </div>
+      <button type="button" class="btn btn-primary">LOAD MORE</button>
     </div>
   </main>
   <TheFooter></TheFooter>
@@ -35,5 +37,14 @@ export default {
 <style>
 main {
   background-color: #1c1c1c;
+}
+
+.btn {
+  border-radius: 0;
+}
+
+.hero {
+  background-image: url(./assets/img/jumbotron.jpg);
+  height: 400px;
 }
 </style>
