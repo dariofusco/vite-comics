11de4cb9633651e03f63fd5a02ae774aaa4bb067
dariofusco <fdario@hotmail.it>
2023-08-23 14:24:51 +0200

    inserisce card fumetti

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <script>
 import TheHeader from './components/TheHeader.vue';
 import TheFooter from './components/TheFooter.vue';
+import ComicsCard from './components/ComicsCard.vue';
+import comicsList from './db/comics';
+
 export default {
   components: {
     TheHeader,
     TheFooter,
+    ComicsCard,
+  },
+  data() {
+    return {
+      comicsList,
+    };
   },
 };
 </script>
 
 <template>
   <TheHeader></TheHeader>
-  <main></main>
+  <main>
+    <div class="container py-4">
+      <div class="row">
+        <div class="col" v-for="comic in comicsList">
+          <ComicsCard :imgSrc="comic.thumb" :comicsType="comic.type"></ComicsCard>
+        </div>
+      </div>
+    </div>
+  </main>
   <TheFooter></TheFooter>
 </template>
 
-<style></style>
+<style>
+main {
+  background-color: #1c1c1c;
+}
+</style>
---
+++ src/components/ComicsCard.vue
@@ -0,0 +1,35 @@
+<script>
+
+export default {
+    props: {
+        imgSrc: String,
+        comicsType: String,
+    },
+};
+
+</script>
+
+<template>
+<div class="card">
+    <img class="card-img-top" :src="imgSrc" alt="">
+    <div class="card-body">
+        <p class="card-text">{{ comicsType }}</p>
+    </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    border: 0;
+    width: fit-content;
+    background-color: #1c1c1c;
+}
+.card-img-top {
+    height: 170px;
+    width: 170px;
+}
+
+.card-text {
+    color: white;
+}
+</style>
